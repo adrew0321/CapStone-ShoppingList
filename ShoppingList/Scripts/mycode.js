e43ddc3a382cc356258d2f891976fb0f5a28781c
A'Keem Drew <adrew0321@gmail.com>
2018-08-14 08:20:44 -0400

    first commit

---
+++ ShoppingList/Scripts/mycode.js
@@ -0,0 +1,16 @@
+﻿var item = ["16oz. Pepsi", "2 Liter Pepsi", "16oz. Dr.Pepper", "2 Liter Dr.Pepper", "16oz. Water", "2 Liter Water"];
+
+var price = [1.29, 1.59, 1.29, 1.59, 1.00, 1.29];
+
+function printList() {
+
+}
+
+
+
+function addToList(name, price, quantity)
+{
+	console.log(name, price, quantity);
+
+
+}
